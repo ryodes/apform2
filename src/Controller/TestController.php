8c1802382b950b4ca548130441d23f52596925e1
kacimi <k2acim31@gmail.com>
2020-12-04 17:30:34 +0100

    nouvelle table + test

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,11 +20,15 @@ class TestController extends AbstractController
     /**
      * @Route("/test", name="test")
      */
-    public function test()
+    public function test(ProductRepository $productRepository)
     {
+        $count = $productRepository->count(['price' => 150]);
+        $product = $productRepository->findAll();
         return $this->render('test.html.twig', [
             'tab' => [1,2,3,5,7,8,9],
             'tab2' => ['toto','tata','titi','tete'],
+            'count' => $count,
+            'products' => $product,
         ]);
     }
 }
