8c0527a3f40ef802376e7850528dbfc647eb2e18
kacimi <k2acim31@gmail.com>
2020-12-04 12:21:04 +0100

    first commit in new branch

---
+++ src/Controller/TestController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test()
+    {
+        return $this->render('test.html.twig', [
+            'tab' => [1,2,3,5,7,8,9]
+        ]);
+    }
+}
